de19e9bb43d3e5e6550e2b3baaba1eb981e5afe9
LIER LIER <1113093541@qq.com>
2022-07-20 23:16:03 +0800

    增加了蛇眼睛

--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -23,6 +23,22 @@ export class Snake extends AcGameObject {
 
         this.step = 0  // 当前回合数
         this.eps = 1e-2  // 允许的误差
+
+        this.eye_direction = 0
+        if (this.id === 1) this.eye_direction = 2
+
+        this.eye_dx = [
+            [-1, 1],
+            [1, 1],
+            [1, -1],
+            [-1, -1]
+        ]
+        this.eye_dy = [
+            [-1, -1],
+            [-1, 1],
+            [1, 1],
+            [1, -1]
+        ]
     }
 
     start() {
@@ -43,6 +59,7 @@ export class Snake extends AcGameObject {
     next_step() {
         const d = this.direction
         this.next_cell = new Cell(this.cells[0].r + this.dr[d], this.cells[0].c + this.dc[d])
+        this.eye_direction = d
         this.direction = -1  // 清空操作
         this.status = "move"
         this.step++
@@ -119,6 +136,17 @@ export class Snake extends AcGameObject {
             } else {
                 ctx.fillRect(Math.min(a.x, b.x) * L, a.y  * L - size / 2, Math.abs(a.x - b.x) * L, size)
             }
+        }
+
+        // 蛇眼睛
+        const eye_r = L * 0.05
+        ctx.fillStyle = "black"
+        for (let i = 0; i < 2; i++) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.15) * L;
+            ctx.beginPath()
+            ctx.arc(eye_x, eye_y, eye_r, 0, Math.PI * 2)
+            ctx.fill()
         }
     }
 }
